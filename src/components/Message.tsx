31aacb03da075b11e1c71313a1838404bbf1d0e6
Cyprian Zdebski <cyprianz5mail@gmail.com>
2025-02-26 23:06:23 +0100

    add and render basic Counter preact component

---
+++ src/components/Message.tsx
@@ -0,0 +1,5 @@
+import type { ComponentChildren } from "preact";
+
+export default function Message({ children }: { children: ComponentChildren }) {
+  return <div class="message">{children}</div>;
+}
