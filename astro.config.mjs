909ae20478116a625b0538583eb33dd0dd79dfce
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2025-03-02 22:58:31 +0100

    add deploy action(?)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bartshoot.github.io",
+  base: "fknexe",
   integrations: [react()],
 
   vite: {
