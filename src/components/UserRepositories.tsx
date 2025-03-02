8ea7457daaa7225aead4f7b47c86463957db237c
Cyprian Zdebski <cyprianz5mail@gmail.com>
2025-03-02 21:01:48 +0100

    port UserRepositories to react

--- src/components/UserRepositories.tsx
+++ src/components/UserRepositories.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useState } from "react";
 import { useQueryState, parseAsString } from "nuqs";
 import { GitHubClient } from "../clients/GithubClient";
 import { Button } from "./Button";
