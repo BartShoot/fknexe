17e604603b8b085a82d3a94dcb1045537e629d7f
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2025-03-06 22:26:35 +0100

    add synonyms and extensions ranking packages

--- src/services/PackageService.ts
+++ src/services/PackageService.ts
@@ -1,6 +1,64 @@
 import { GitHubClient as client } from "@/clients/GithubClient.ts";
 import type { IAsset } from "@/lib/types";
 import { UAParser } from "ua-parser-js";
+import { OS, CPU } from "ua-parser-js/enums";
+
+const osSynonyms: Record<string, string[]> = {
+  [OS.WINDOWS.toLowerCase()]: [
+    "windows",
+    "win",
+    "win32",
+    "win64",
+    "winnt",
+    "win10",
+    "win11",
+    "msie",
+    "msedge",
+  ],
+  [OS.MACOS.toLowerCase()]: [
+    "darwin",
+    "mac",
+    "macos",
+    "osx",
+    "mac os x",
+    "apple",
+    "mac_powerpc",
+    "macintosh",
+    "apple",
+    "catalina",
+    "monterey",
+    "ventura",
+    "sonoma",
+  ],
+  [OS.LINUX.toLowerCase()]: [
+    "linux",
+    "gnu linux",
+    "ubuntu",
+    "debian",
+    "fedora",
+    "centos",
+    "redhat",
+    "rhel",
+    "opensuse",
+    "suse",
+    "arch",
+    "gentoo",
+    "x11",
+  ],
+};
+
+const archSynonyms: Record<string, string[]> = {
+  [CPU.X86_64.toLowerCase()]: ["x86_64", "x64", "amd64"],
+  [CPU.ARM_64.toLowerCase()]: ["arm64", "aarch64"],
+  [CPU.X86.toLowerCase()]: ["x86", "ia32"],
+  [CPU.ARM_HF.toLowerCase()]: ["armhf", "armv7"],
+};
+
+const exclusiveExtensions: Record<string, string[]> = {
+  [OS.WINDOWS.toLowerCase()]: [".exe", ".msi", ".bat", ".cmd"],
+  [OS.MACOS.toLowerCase()]: [".dmg", ".pkg", ".app"],
+  [OS.LINUX.toLowerCase()]: [".deb", ".rpm", ".appimage"],
+};
 
 class PackageService {
   async getRankedPackages(user: string, repo: string, userAgent: string) {
@@ -36,15 +94,48 @@ class PackageService {
       partial_match: [] as string[],
       conflicts: [] as string[],
     };
-    if (p.name.includes(ua.os.name?.toLowerCase() || "")) {
-      score++;
-      matches.exact_match.push("OS");
+    const packageName = p.name.toLowerCase();
+    const osName = ua.os.name?.toLowerCase() || "";
+    const currentOsSynonyms = osSynonyms[osName];
+    if (currentOsSynonyms) {
+      for (const synonym of currentOsSynonyms) {
+        if (packageName.includes(synonym)) {
+          score++;
+          matches.exact_match.push("OS");
+        }
+      }
+    } else {
+      if (packageName.includes(osName)) {
+        score++;
+        matches.exact_match.push("OS");
+      }
     }
-    if (p.name.includes(ua.cpu.architecture?.toLowerCase() || "")) {
-      score++;
-      matches.exact_match.push("Architecture");
+
+    const archName = ua.cpu.architecture?.toLowerCase() || "";
+    const currentArchSynonyms = archSynonyms[archName];
+    if (currentArchSynonyms) {
+      for (const synonym of currentArchSynonyms) {
+        if (packageName.includes(synonym)) {
+          score++;
+          matches.exact_match.push("Architecture");
+        }
+      }
+    } else {
+      if (packageName.includes(archName)) {
+        score++;
+        matches.exact_match.push("Architecture");
+      }
     }
-    // TODO: file extensions, fuzzy search/synonyms
+
+    const currentOsExtensions = exclusiveExtensions[osName];
+    if (currentOsExtensions) {
+      for (const extension of currentOsExtensions) {
+        if (packageName.includes(extension)) {
+          score++;
+          matches.exact_match.push("Extension");
+        }
+      }
+    }
     return {
       score,
       matches,
