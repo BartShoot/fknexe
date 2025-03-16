57b4c2509aadc0efacfce39b367e9d1d3f376846
Cyprian Zdebski <cyprianz5mail@gmail.com>
2025-03-16 01:41:23 +0100

    Adjust layouts

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -7,14 +7,12 @@ interface AppProps {
 
 export const App = ({ children }: AppProps) => {
   return (
-    <ThemeProvider defaultTheme='dark' storageKey='ui-theme'>
+    <ThemeProvider defaultTheme='light' storageKey='ui-theme'>
       <div className='flex flex-col min-h-full'>
-        <main className='w-full flex-1'>
-          <div className='fixed top-2 right-2'>
-            <ModeToggle />
-          </div>
-          {children}
-        </main>
+        <div className='fixed top-2 right-2'>
+          <ModeToggle />
+        </div>
+        <main className='flex flex-col items-center flex-1'>{children}</main>
         <footer className='text-center text-gray-500 text-sm py-4'>
           brought to you by smelly nerds
         </footer>
