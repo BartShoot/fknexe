ec6b49ee29000e480e1d5f46c4295a02b72431f9
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2025-03-27 18:08:53 +0100

    let me use the damn private method

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,12 +5,12 @@ export type ISyncFunction = (...args: any[]) => any
 export type IASyncFunction = (...args: any[]) => Promise<any>
 export type IFunction = ISyncFunction | IASyncFunction
 export type IAsset = components['schemas']['release-asset']
-export type IMatchResult ={
-  score: number;
+export interface IMatchResult {
+  score: number
   matches: {
-    exact_match: string[];
-    partial_match: string[];
-    conflicts: string[];
-  };
+    exact_match: string[]
+    partial_match: string[]
+    conflicts: string[]
+  }
 }
 export type OSType = (typeof OS)[keyof typeof OS]
