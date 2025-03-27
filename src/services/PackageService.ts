a499b7768f26bb9d4c29089cd8f1a10d7b763d29
Bartosz Dobija <dobija.bartosz@gmail.com>
2025-03-27 20:13:58 +0100

    Package ranking test (#30)
    * add test
    
    * improve ranking service according to tests
    
    * add expected matches, exhaustive os switch, find darwin win problem
    
    * refactor rankPackages for easier testing
    
    * implement better matching algorithm
    
    * refactoring. reordering and making methods private
    
    * industrial revolution and it's consequences

--- src/services/PackageService.ts
+++ src/services/PackageService.ts
@@ -1,6 +1,153 @@
 import { UAParser } from 'ua-parser-js'
 import { CPU, OS } from 'ua-parser-js/enums'
 import { GithubApi, type GithubResponse } from '@/clients/github/api'
+import type { IAsset, IMatchResult, OSType } from '@/lib/types'
+
+class PackageService {
+  async getRankedPackages(owner: string, repo: string, userAgent: string) {
+    const latestRelease: GithubResponse['getLatestRelease'] = await GithubApi.getLatestRelease({
+      owner,
+      repo,
+    })
+    const parsedUA = UAParser(userAgent)
+    const rankedPackages = this.getSortedRankedPackages(latestRelease.assets, parsedUA)
+    delete (latestRelease as { assets?: IAsset[] }).assets
+    return { latestRelease, rankedPackages }
+  }
+
+  private getSortedRankedPackages(packages: IAsset[], ua: UAParser.IResult) {
+    return this.rankPackages(
+      packages.flatMap((p) => p.name),
+      ua,
+    )
+      .map((ranked) => {
+        const originalPackage = packages.find((pkg) => pkg.name === ranked.packageName)!!
+        return {
+          ...originalPackage,
+          matchInfo: ranked.matchInfo,
+        }
+      })
+      .sort((a, b) => {
+        const bHasOS = b.matchInfo.matches.exact_match.includes('OS')
+        const aHasOS = a.matchInfo.matches.exact_match.includes('OS')
+        return (
+          b.matchInfo.score - a.matchInfo.score ||
+          ((bHasOS && !aHasOS) || (!bHasOS && aHasOS) ? 1 : -1) ||
+          b.download_count - a.download_count
+        )
+      })
+  }
+
+  rankPackages(packageNames: string[], ua: UAParser.IResult) {
+    return packageNames.map((name) => {
+      const matchInfo = this.matchInfo(name, ua)
+      return { packageName: name, matchInfo }
+    })
+  }
+
+  private matchInfo(packageName: string, ua: UAParser.IResult): IMatchResult {
+    const score = { os: 0, arch: 0, ext: 0, conflicts: 0 }
+    const matches = {
+      exact_match: [] as string[],
+      partial_match: [] as string[],
+      conflicts: [] as string[],
+    }
+    const osName = getCurrentOS(ua.os).toLowerCase()
+    const archName = ua.cpu.architecture?.toLowerCase() ?? ''
+
+    const osScore = 5
+    const osMatch = this.getOsMatch(packageName, osName)
+    this.matchAgainstSynonyms(packageName, osSynonyms, osName, osScore, matches, osMatch, 'OS')
+    score.os = osScore
+
+    const archScore = 3
+    const archMatch = this.getArchitectureMatch(packageName, archName)
+    this.matchAgainstSynonyms(
+      packageName,
+      archSynonyms,
+      archName,
+      archScore,
+      matches,
+      archMatch,
+      'Architecture',
+    )
+    score.arch = archScore
+
+    const extScore = 2
+    const extMatch = this.getExtensionMatch(packageName, osName)
+    this.matchAgainstSynonyms(
+      packageName,
+      exclusiveExtensions,
+      osName,
+      extScore,
+      matches,
+      extMatch,
+      'Extension',
+    )
+    score.ext = extScore
+
+    return {
+      score: score.os + score.arch + score.ext + score.conflicts,
+      matches,
+    }
+  }
+
+  private getOsMatch(packageName: string, osName: string) {
+    return this.findBestMatch(packageName, [osName, ...(osSynonyms[osName] ?? [])])
+  }
+
+  private getArchitectureMatch(packageName: string, archName: string) {
+    return this.findBestMatch(packageName, [archName, ...(archSynonyms[archName] ?? [])])
+  }
+
+  private getExtensionMatch(packageName: string, osName: string) {
+    return (exclusiveExtensions[osName] ?? []).find((ext) => packageName.endsWith(ext))
+  }
+
+  private matchAgainstSynonyms(
+    packageName: string,
+    synonyms: Record<string, string[]>,
+    currentName: string,
+    score: number,
+    matches: { exact_match: string[]; conflicts: string[] },
+    match: string | undefined,
+    type: string,
+  ) {
+    const allConflicts = Object.entries(synonyms)
+      .filter(([key]) => key !== currentName)
+      .flatMap(([, synonyms]) => synonyms)
+    const conflictMatch = this.findBestMatch(packageName, allConflicts)
+    const longestMatch = this.getLongestMatch([match, conflictMatch])
+
+    if (longestMatch) {
+      if (longestMatch === match) {
+        score *= 1
+        matches.exact_match.push(type)
+      } else if (longestMatch === conflictMatch) {
+        score *= 1
+        matches.conflicts.push(type)
+      }
+      return true
+    }
+    return false
+  }
+
+  private findBestMatch(packageName: string, potentialMatches: string[]): string | undefined {
+    const matches = potentialMatches.filter((match) => packageName.includes(match))
+    return matches.sort((a, b) => b.length - a.length)[0]
+  }
+
+  private getLongestMatch(matches: (string | undefined)[]): string | undefined {
+    return matches.reduce((longest, current) => {
+      if (!current) return longest
+      if (!longest) return current
+      return current.length > longest.length ? current : longest
+    }, undefined)
+  }
+}
+
+const defaultPackageService = new PackageService()
+export { defaultPackageService as PackageService }
 
 const osSynonyms: Record<string, string[]> = {
   [OS.WINDOWS.toLowerCase()]: [
@@ -59,147 +206,114 @@ const exclusiveExtensions: Record<string, string[]> = {
   [OS.LINUX.toLowerCase()]: ['.deb', '.rpm', '.appimage'],
 }
 
-class PackageService {
-  async getRankedPackages(owner: string, repo: string, userAgent: string) {
-    const latestRelease: GithubResponse['getLatestRelease'] = await GithubApi.getLatestRelease({
-      owner,
-      repo,
-    })
-    const parsedUA = UAParser(userAgent)
-    const rankedPackages = this.rankPackages(latestRelease.assets, parsedUA).sort((a, b) => {
-      const bHasOS = b.matchInfo.matches.exact_match.includes('OS')
-      const aHasOS = a.matchInfo.matches.exact_match.includes('OS')
-      return (
-        b.matchInfo.score - a.matchInfo.score ||
-        ((bHasOS && !aHasOS) || (!bHasOS && aHasOS) ? 1 : -1) ||
-        b.package.downloadCount - a.package.downloadCount
-      )
-    })
-    delete (latestRelease as { assets?: any[] }).assets
-    return { latestRelease, rankedPackages }
-  }
-
-  // TODO: proper types
-  rankPackages(packages: any[], ua: UAParser.IResult) {
-    return packages.map((pkg) => {
-      // TODO: take package name from name or common parts
-      const matchInfo = this.matchInfo(pkg, ua)
-      return { package: pkg, matchInfo }
-    })
-  }
-
-  matchInfo(p: any, ua: UAParser.IResult) {
-    let score = 0
-    const matches = {
-      exact_match: [] as string[],
-      partial_match: [] as string[],
-      conflicts: [] as string[],
-    }
-    const packageName = p.name.toLowerCase()
-    const osName = ua.os.name?.toLowerCase() ?? ''
-    const currentOsSynonyms = osSynonyms[osName]
-
-    let matchesOtherOs = false
-    for (const [otherOs, synonyms] of Object.entries(osSynonyms)) {
-      if (otherOs !== osName) {
-        for (const synonym of synonyms) {
-          if (packageName.includes(synonym)) {
-            matchesOtherOs = true
-            break
-          }
-        }
-        if (matchesOtherOs) break
-      }
-    }
-
-    if (matchesOtherOs) {
-      // TODO: darwin vs win
-      score--
-      matches.conflicts.push('OS')
-    } else {
-      if (currentOsSynonyms) {
-        for (const synonym of currentOsSynonyms) {
-          if (packageName.includes(synonym)) {
-            score++
-            matches.exact_match.push('OS')
-          }
-        }
-      } else {
-        if (packageName.includes(osName)) {
-          score++
-          matches.exact_match.push('OS')
-        }
-      }
-    }
-
-    const archName = ua.cpu.architecture?.toLowerCase() ?? ''
-    let matchesOtherArch = false
-    for (const [otherArchs, synonyms] of Object.entries(archSynonyms)) {
-      if (otherArchs !== archName) {
-        for (const synonym of synonyms) {
-          if (packageName.includes(synonym)) {
-            matchesOtherArch = true
-            break
-          }
-        }
-        if (matchesOtherArch) break
-      }
-    }
-
-    if (matchesOtherArch) {
-      // TODO: x86 vs x86_64
-      score--
-      matches.conflicts.push('Architecture')
-    } else {
-      const currentArchSynonyms = archSynonyms[archName]
-      if (currentArchSynonyms) {
-        for (const synonym of currentArchSynonyms) {
-          if (packageName.includes(synonym)) {
-            score++
-            matches.exact_match.push('Architecture')
-          }
-        }
-      } else {
-        if (packageName.includes(archName)) {
-          score++
-          matches.exact_match.push('Architecture')
-        }
-      }
-    }
-
-    const currentOsExtensions = exclusiveExtensions[osName]
-    let matchesOtherExtension = false
-    for (const [extensionSource, extensions] of Object.entries(exclusiveExtensions)) {
-      if (extensionSource !== osName) {
-        for (const extension of extensions) {
-          if (packageName.includes(extension)) {
-            // TODO changes to ends with
-            matchesOtherExtension = true
-            break
-          }
-        }
-        if (matchesOtherExtension) break
-      }
-    }
-    if (matchesOtherExtension) {
-      score--
-      matches.conflicts.push('Extension')
-    } else {
-      if (currentOsExtensions) {
-        for (const extension of currentOsExtensions) {
-          if (packageName.includes(extension)) {
-            score++
-            matches.exact_match.push('Extension')
-          }
-        }
-      }
-    }
-    return {
-      score,
-      matches,
-    }
-  }
+function toOSType(value: string): OSType | undefined {
+  return (Object.values(OS) as string[]).includes(value) ? (value as OSType) : undefined
 }
 
-const defaultPackageService = new PackageService()
-export { defaultPackageService as PackageService }
+function getCurrentOS(os: UAParser.IOS) {
+  if (!os.name) return ''
+  const osType = toOSType(os.name)
+  switch (osType) {
+    case OS.WINDOWS:
+      return OS.WINDOWS.toString()
+    case OS.MACOS:
+      return OS.MACOS.toString()
+    case OS.FREEBSD:
+    case OS.GHOSTBSD:
+      return OS.FREEBSD.toString()
+    case OS.OPENBSD:
+    case OS.NETBSD:
+      return OS.OPENBSD.toString()
+    case OS.LINUX:
+    case OS.UBUNTU:
+    case OS.GNU:
+    case OS.GENTOO:
+    case OS.AIX:
+    case OS.ARCH:
+    case OS.DEBIAN:
+    case OS.ELEMENTARY_OS:
+    case OS.FEDORA:
+    case OS.SUSE:
+    case OS.CENTOS:
+    case OS.DRAGONFLY:
+    case OS.FIREFOX_OS:
+    case OS.HP_UX:
+    case OS.HURD:
+    case OS.KUBUNTU:
+    case OS.LINPUS:
+    case OS.LINSPIRE:
+    case OS.MAGEIA:
+    case OS.MANDRIVA:
+    case OS.MANJARO:
+    case OS.MEEGO:
+    case OS.MINIX:
+    case OS.MINT:
+    case OS.PCLINUXOS:
+    case OS.RASPBIAN:
+    case OS.REDHAT:
+    case OS.SABAYON:
+    case OS.SERENITYOS:
+    case OS.SLACKWARE:
+    case OS.SOLARIS:
+    case OS.UNIX:
+    case OS.VECTORLINUX:
+    case OS.ZENWALK:
+      return OS.LINUX.toString()
+    //misc
+    case OS.NINTENDO:
+    case OS.NETRANGE:
+    case OS.NETTV:
+    case OS.OPENHARMONY:
+    case OS.OPENVMS:
+    case OS.PLAYSTATION:
+    case OS.WEBOS:
+    case OS.WINDOWS_IOT:
+    case OS.XBOX:
+      return ''
+    //mobile
+    case OS.WINDOWS_MOBILE:
+    case OS.ANDROID:
+    case OS.ANDROID_X86:
+    case OS.BADA:
+    case OS.BLACKBERRY:
+    case OS.CHROME_OS:
+    case OS.CHROMECAST:
+    case OS.CHROMECAST_ANDROID:
+    case OS.CHROMECAST_FUCHSIA:
+    case OS.CHROMECAST_LINUX:
+    case OS.CHROMECAST_SMARTSPEAKER:
+    case OS.CONTIKI:
+    case OS.DEEPIN:
+    case OS.FUCHSIA:
+    case OS.HAIKU:
+    case OS.HARMONYOS:
+    case OS.IOS:
+    case OS.PALM:
+    case OS.PICO:
+    case OS.RIM_TABLET_OS:
+    case OS.SAILFISH:
+    case OS.SYMBIAN:
+    case OS.TIZEN:
+    case OS.UBUNTU_TOUCH:
+    case OS.WATCHOS:
+    case OS.WINDOWS_PHONE:
+      return ''
+    //legacy
+    case OS.BEOS:
+    case OS.AMIGA_OS:
+    case OS.JOLI:
+    case OS.KAIOS:
+    case OS.MAEMO:
+    case OS.MORPH_OS:
+    case OS.QNX:
+    case OS.OS2:
+    case OS.PC_BSD:
+    case OS.PLAN9:
+    case OS.RISC_OS:
+    case OS.SERIES40:
+    case undefined:
+      return ''
+    default:
+      return osType satisfies never
+  }
+}
---
+++ src/tests/PackageService.test.ts
@@ -0,0 +1,97 @@
+import { UAParser } from 'ua-parser-js'
+import { describe, expect, it } from 'vitest'
+import { PackageService } from '@/services/PackageService.ts'
+import { getTestCases } from '@/tests/data/packages.mock.ts'
+
+describe('correct matches', () => {
+  const testCases = getTestCases()
+  testCases.forEach((testScenario) => {
+    testScenario.cases.forEach((thisCase) => {
+      it(`${testScenario.name} - ${thisCase.id}`, () => {
+        const userAgent = UAParser(testScenario.ua)
+        console.log({ name: userAgent.os.name, arch: userAgent.cpu.architecture })
+        const ranked = PackageService.rankPackages(thisCase.names, userAgent)
+        console.log(
+          ranked
+            .sort((a, b) => b.matchInfo.score - a.matchInfo.score)
+            .map(
+              (r) =>
+                r.packageName +
+                ' - score: ' +
+                r.matchInfo.score +
+                ' exactMatch: {' +
+                r.matchInfo.matches.exact_match.join(', ') +
+                '}',
+            ),
+        )
+        ranked.forEach((p) => {
+          const exactMatch = p.matchInfo.matches.exact_match
+          const arrLength = exactMatch.length
+          const uniqueCount = new Set(exactMatch).size
+          expect(arrLength).toBe(uniqueCount)
+        })
+        if (thisCase.expectedMatches) {
+          for (const [fileName, expectedExactMatches] of Object.entries(thisCase.expectedMatches)) {
+            const matchedPackage = ranked.find((p) => p.packageName === fileName)
+
+            if (matchedPackage) {
+              const actualMatches = matchedPackage.matchInfo.matches.exact_match
+
+              // Compare regardless of order by using sets
+              const actualMatchesSet = new Set(actualMatches)
+              const expectedMatchesSet = new Set(expectedExactMatches)
+
+              // Use a custom error message with full context
+              if (actualMatchesSet.size !== expectedMatchesSet.size) {
+                const extraItems = [...actualMatchesSet].filter(
+                  (item) => !expectedMatchesSet.has(item),
+                )
+                const missingItems = [...expectedMatchesSet].filter(
+                  (item) => !actualMatchesSet.has(item),
+                )
+
+                const errorMessage = `
+File: ${fileName}
+Expected ${expectedMatchesSet.size} exact matches: [${[...expectedMatchesSet].join(', ')}]
+Actual ${actualMatchesSet.size} exact matches: [${[...actualMatchesSet].join(', ')}]
+${extraItems.length > 0 ? `Extra items: [${extraItems.join(', ')}]` : ''}
+${missingItems.length > 0 ? `Missing items: actualMatches[${missingItems.join(', ')}]` : ''}
+${JSON.stringify(matchedPackage.matchInfo.matches)}
+`
+                expect.fail(errorMessage)
+              }
+
+              // Check each expected item is in actual matches
+              for (const expectedMatch of expectedMatchesSet) {
+                if (!actualMatchesSet.has(expectedMatch)) {
+                  expect.fail(`
+File: ${fileName}
+Missing expected match: "${expectedMatch}"
+Expected matches: [${[...expectedMatchesSet].join(', ')}]
+Actual matches: [${[...actualMatchesSet].join(', ')}]
+`)
+                }
+              }
+
+              // Check for extra items
+              for (const actualMatch of actualMatchesSet) {
+                if (!expectedMatchesSet.has(actualMatch)) {
+                  expect.fail(`
+File: ${fileName}
+Extra unexpected match: "${actualMatch}"
+Expected matches: [${[...expectedMatchesSet].join(', ')}]
+Actual matches: [${[...actualMatchesSet].join(', ')}]
+`)
+                }
+              }
+            } else {
+              // Fail test if we expected matches for a file that wasn't found
+              expect.fail(`Expected file ${fileName} not found in ranked packages`)
+            }
+          }
+        }
+        expect(ranked.at(0)!.matchInfo.score.valueOf() > 1)
+      })
+    })
+  })
+})
