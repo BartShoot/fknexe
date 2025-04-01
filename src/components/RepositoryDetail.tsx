0fa75f6dc2fdffa4b59ac112c18089df2ae956a1
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2025-04-01 22:00:44 +0200

    fix layout

--- src/components/RepositoryDetail.tsx
+++ src/components/RepositoryDetail.tsx
@@ -112,101 +112,103 @@ function _RepositoryDetail() {
     <div
       className={`container mx-auto p-4 md:p-6 rounded-lg shadow-md ${theme === 'dark' ? 'dark' : ''}`}
     >
-      {/* Top Section: Release Body and First Package */}
-      <div className={`mb-6 pb-4 flex flex-col md:flex-row gap-6 border-b dark:border-zinc-800`}>
-        {/* Release Body */}
-        <div className='md:w-2/3'>
-          <h2 className={`text-2xl font-bold mb-2 dark:text-white`}>
-            Release Notes: {latestRelease.name} ({latestRelease.tag_name})
-          </h2>
-          <p className={`text-sm mb-3 text-gray-600 dark:text-gray-400`}>
-            {latestRelease.published_at && (
-              <span>
-                Published on: {new Date(latestRelease.published_at).toLocaleDateString()} by{' '}
-              </span>
-            )}
-            <a
-              href={latestRelease.author.html_url}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-blue-600 hover:underline dark:text-blue-400'
-            >
-              {latestRelease.author.login}
-            </a>
-          </p>
-          {latestRelease.body ?
-            <div className='typography max-w-none bg-zinc-50 border-zinc-200 dark:bg-zinc-900 dark:border-zinc-800 px-4 py-1 rounded-md'>
-              <Markdown skipHtml remarkPlugins={[remarkGfm]}>
-                {latestRelease.body}
-              </Markdown>
-            </div>
-          : <div
-              className={`border border-dashed rounded-md p-6 min-h-[300px] flex items-center justify-center border-gray-300 bg-gray-50 text-gray-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-gray-400`}
-            >
-              <p>No release notes found for this release.</p>
-            </div>
-          }
-        </div>
-
-        {/* First Ranked Package */}
-        {firstPackage && (
-          <div
-            className={`md:w-1/3 p-4 rounded-md border bg-blue-50 border-blue-200 dark:bg-blue-900 dark:border-blue-800`}
-          >
-            <h3 className={`text-2xl font-semibold mb-2 text-blue-800 dark:text-blue-200`}>
-              Recommended Download
-            </h3>
-            {parsedUA && (
-              <p className={`text-sm mb-3 text-gray-600 dark:text-gray-300`}>
-                For your {osName} system{' '}
-                {parsedUA.cpu.architecture ? `with ${parsedUA.cpu.architecture} CPU` : ''} we
-                recommend:
-              </p>
-            )}
-            <div className='flex flex-col'>
-              <p className={`text-lg font-medium break-words text-gray-800 dark:text-gray-200`}>
-                {firstPackage.name}
-              </p>
-              {firstPackage.matchInfo && renderMatchTags(firstPackage.matchInfo)}
-            </div>
-            <p className={`text-sm mb-3 mt-3 text-gray-600 dark:text-gray-300`}>
-              Downloads: {firstPackage.download_count.toLocaleString()}
-            </p>
-            <a
-              href={firstPackage.browser_download_url}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full text-sm transition duration-150 ease-in-out w-full text-center dark:bg-blue-500 dark:hover:bg-blue-400'
-              download
-            >
-              Download ({formatBytes(firstPackage.size)})
-            </a>
-          </div>
-        )}
-      </div>
-
-      {/* Bottom Section: Two Columns */}
+      {/* Main Content: Two Columns */}
       <div className='flex flex-col md:flex-row gap-6'>
-        {/* Left Column: README */}
-        <div className='w-full md:w-2/3'>
-          <h3 className={`text-xl font-semibold mb-3 text-gray-700 dark:text-gray-300`}>README</h3>
-          {readme ?
-            <div className='typography max-w-none bg-zinc-50 border-zinc-200 dark:bg-zinc-900 dark:border-zinc-800 px-8 py-1 rounded-md'>
-              <Markdown skipHtml remarkPlugins={[remarkGfm]}>
-                {readme.content}
-              </Markdown>
-            </div>
-          : <div
-              className={`border border-dashed rounded p-6 min-h-[300px] flex items-center justify-center border-gray-300 bg-gray-50 text-gray-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-gray-400`}
-            >
-              <p>No README found for this repository.</p>
-            </div>
-          }
+        {/* Left Column */}
+        <div className='md:w-2/3 flex flex-col gap-6'>
+          {/* Release Notes */}
+          <div className={`pb-4 border-b dark:border-zinc-800`}>
+            <h2 className={`text-2xl font-bold mb-2 dark:text-white`}>
+              Release Notes: {latestRelease.name} ({latestRelease.tag_name})
+            </h2>
+            <p className={`text-sm mb-3 text-gray-600 dark:text-gray-400`}>
+              {latestRelease.published_at && (
+                <span>
+                  Published on: {new Date(latestRelease.published_at).toLocaleDateString()} by{' '}
+                </span>
+              )}
+              <a
+                href={latestRelease.author.html_url}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-blue-600 hover:underline dark:text-blue-400'
+              >
+                {latestRelease.author.login}
+              </a>
+            </p>
+            {latestRelease.body ?
+              <div className='typography max-w-none bg-zinc-50 border-zinc-200 dark:bg-zinc-900 dark:border-zinc-800 px-4 py-1 rounded-md'>
+                <Markdown skipHtml remarkPlugins={[remarkGfm]}>
+                  {latestRelease.body}
+                </Markdown>
+              </div>
+            : <div
+                className={`border border-dashed rounded-md p-6 min-h-[300px] flex items-center justify-center border-gray-300 bg-gray-50 text-gray-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-gray-400`}
+              >
+                <p>No release notes found for this release.</p>
+              </div>
+            }
+          </div>
+
+          {/* Readme */}
+          <div className={`pb-4 border-b dark:border-zinc-800`}>
+            <h3 className={`text-xl font-semibold mb-3 text-gray-700 dark:text-gray-300`}>
+              README
+            </h3>
+            {readme ?
+              <div className='typography max-w-none bg-zinc-50 border-zinc-200 dark:bg-zinc-900 dark:border-zinc-800 px-8 py-1 rounded-md'>
+                <Markdown skipHtml remarkPlugins={[remarkGfm]}>
+                  {readme.content}
+                </Markdown>
+              </div>
+            : <div
+                className={`border border-dashed rounded p-6 min-h-[300px] flex items-center justify-center border-gray-300 bg-gray-50 text-gray-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-gray-400`}
+              >
+                <p>No README found for this repository.</p>
+              </div>
+            }
+          </div>
         </div>
 
-        {/* Right Column: Packages */}
-        <div className='w-full md:w-1/3'>
-          {/* Rest of Ranked Packages */}
+        {/* Right Column */}
+        <div className='md:w-1/3 flex flex-col gap-6'>
+          {/* First Ranked Package */}
+          {firstPackage && (
+            <div
+              className={`p-4 rounded-md border bg-blue-50 border-blue-200 dark:bg-blue-900 dark:border-blue-800`}
+            >
+              <h3 className={`text-2xl font-semibold mb-2 text-blue-800 dark:text-blue-200`}>
+                Recommended Download
+              </h3>
+              {parsedUA && (
+                <p className={`text-sm mb-3 text-gray-600 dark:text-gray-300`}>
+                  For your {osName} system{' '}
+                  {parsedUA.cpu.architecture ? `with ${parsedUA.cpu.architecture} CPU` : ''} we
+                  recommend:
+                </p>
+              )}
+              <div className='flex flex-col'>
+                <p className={`text-lg font-medium break-words text-gray-800 dark:text-gray-200`}>
+                  {firstPackage.name}
+                </p>
+                {firstPackage.matchInfo && renderMatchTags(firstPackage.matchInfo)}
+              </div>
+              <p className={`text-sm mb-3 mt-3 text-gray-600 dark:text-gray-300`}>
+                Downloads: {firstPackage.download_count.toLocaleString()}
+              </p>
+              <a
+                href={firstPackage.browser_download_url}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full text-sm transition duration-150 ease-in-out w-full text-center dark:bg-blue-500 dark:hover:bg-blue-400'
+                download
+              >
+                Download ({formatBytes(firstPackage.size)})
+              </a>
+            </div>
+          )}
+
+          {/* Other Packages */}
           {otherPackages.length > 0 && (
             <div>
               <h3 className={`text-lg font-semibold mb-3 text-gray-700 dark:text-gray-300`}>
